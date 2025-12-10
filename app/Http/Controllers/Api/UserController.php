16bf5d469d6718032246f025cfc385b2a9fd09e6
haseeb <haseebfayyaz21@gmail.com>
2025-12-11 00:53:58 +0500

    update endpoint

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -17,5 +17,12 @@ class UserController extends Controller
 
         return response()->json($users);
     }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+        return response()->json($user);
+    }
 }
 
